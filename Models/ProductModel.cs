66863fda175cca8583a6dfcfd930eda6dff5fe79
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-05-08 13:21:00 +0200

    Task 3

--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -6,4 +6,5 @@ public class ProductModel
     public string Name { get; set; }
     public string Description { get; set; }
     public float Price { get; set; }
+    public int Stock { get; set; }
 }
--- ViewModels/VendingMachineViewModel.cs
+++ ViewModels/VendingMachineViewModel.cs
@@ -4,11 +4,11 @@ public class VendingMachineViewModel
 {
     private List<ProductModel> products = new List<ProductModel>()
     {
-        new ProductModel { Name = "Boot", Price = 2 },
-        new ProductModel { Name = "Citroen", Price = 5 },
-        new ProductModel { Name = "Deur", Price = 6.6f },
-        new ProductModel { Name = "Egel", Price = 5.33f },
-        new ProductModel { Name = "Hooi", Price = 10.03f },
+        new ProductModel { Name = "Boot", Price = 2, Stock = 2 },
+        new ProductModel { Name = "Citroen", Price = 5, Stock = 12 },
+        new ProductModel { Name = "Deur", Price = 6.6f, Stock = 23 },
+        new ProductModel { Name = "Egel", Price = 5.33f, Stock = 0 },
+        new ProductModel { Name = "Hooi", Price = 10.03f, Stock = 6 },
     };
 
     public List<ProductModel> GetProducts() => products;
